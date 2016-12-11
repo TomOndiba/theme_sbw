09b7e1a8139c4284d342f5bb90f32b372182ca8f
Juho Jaakkola <juho.a.jaakkola@gmail.com>
2016-12-11 17:03:54 +0000

    Enhances footer layout, and replace Donators link
    Fixes #56

--- views/default/sidebar/donate.php
+++ views/default/sidebar/donate.php
@@ -1,17 +1,11 @@
 <?php
 
-// TODO Add this through elgg_echo()
 $content = '<p>We rely on your <a href="https://shop.socialbusinessworld.org/en/13-ethical-products-entire-catalogue">ETHICAL SHOPPING</a>
 at our Marketplace and your DONATIONS via <a href="https://www.paypal.me/socialbusinessworld">Paypal</a> or <a href="https://tag.satispay.com/sbworld">via Satispay</a>.
 
-<p>See <a href="https://socialbusinessworld.org/donation">latest Donators</a>.</p>
+See <a href="https://socialbusinessworld.org/groups/members/74795">latest Donators</a>.</p>
 
-<p><b>Via banktransfer to:</b><br />
-
-Social Business World<br />
-Ethic Bank (Banca Etica)<br />Branch: Ancona (Italy)<br />
-IBAN: IT90O0501802600000000151909<br />
-BIC/SWIFT: CCRTIT2T84A</p>
+<p><b>Via banktransfer to:</b> Social Business World - Ethic Bank (Banca Etica) - Branch: Ancona (Italy) - IBAN: IT90O0501802600000000151909 - BIC/SWIFT: CCRTIT2T84A</p>
 
 <p>Consider supporting us now. Thank you!</p>';
 
